530379ecd4beb10f12de10198dc3815210fc4a6e
semitrivial <alexander@math.ohio-state.edu>
2015-04-01 16:06:51 +0100

    Add 'Include' command

--- Apitest.java
+++ Apitest.java
@@ -11,53 +11,67 @@ import java.lang.StringBuilder;
 
 public class Apitest
 {
+  String filename;
   InputStream is;
   BufferedReader br;
   String line;
-  String href_base;
-  String href_postfix;
-  String prev_result;
-  String prev_href;
-  boolean prev_result_unreadable;
-  int linenum;
-  int sleeptime;
-  int total_queries;
-  int nonproblematic_queries;
+  String href_base = "";
+  String href_postfix = "";
+  String prev_result = null;
+  String prev_href = null;
+  boolean prev_result_unreadable = false;
+  int linenum = 0;
+  int sleeptime = 100;
+  int total_queries = 0;
+  int nonproblematic_queries = 0;
 
   public static void main(String [] args) throws Exception
   {
-    Apitest apitest = new Apitest();
-    apitest.run( args );
-  }
-
-  private void run( String [] args )
-  {
-    if ( !parse_cmdline( args ) )
+    Apitest apitest;
+    try
+    {
+      apitest = new Apitest( args );
+    }
+    catch( BadCommandLineException e )
+    {
       return;
-
-    linenum = 0;
-    href_base = "";
-    href_postfix = "";
-    prev_result = null;
-    prev_result_unreadable = false;
-    total_queries = 0;
-    nonproblematic_queries = 0;
+    }
 
     outln( "Running API commands..." );
     outln( "" );
 
+    apitest.run();
+
+    outln( "Finished running " + apitest.total_queries + " queries." );
+
+    if ( apitest.nonproblematic_queries < apitest.total_queries )
+      outln( "Of those, " + (apitest.total_queries - apitest.nonproblematic_queries) + " had unexpected behavior." );
+    else
+      outln( "All queries behaved as expected." );
+
+  }
+
+  public Apitest( String [] args ) throws BadCommandLineException
+  {
+    if ( !this.parse_cmdline( args ) )
+      throw new BadCommandLineException();
+  }
+
+  public Apitest( String filename ) throws BadCommandLineException
+  {
+    this.filename = filename;
+
+    if ( !this.open_file() )
+      throw new BadCommandLineException();
+  }
+
+  private void run()
+  {
     for ( String line = read_line(); line != null; line = read_line() )
     {
       if ( !parse_line( line ) )
         return;
     }
-
-    outln( "Finished running " + total_queries + " queries." );
-
-    if ( nonproblematic_queries < total_queries )
-      outln( "Of those, " + (total_queries - nonproblematic_queries) + " had unexpected behavior." );
-    else
-      outln( "All queries behaved as expected." );
   }
 
   private boolean parse_line( String line )
@@ -77,6 +91,30 @@ public class Apitest
         parser_error( "Sleeptime must be a non-negative integer" );
         return false;
       }
+
+      return true;
+    }
+
+    if ( line.startsWith( "Include " ) )
+    {
+      Apitest apitest;
+
+      try
+      {
+        apitest = new Apitest( line.substring( "Include ".length() ).trim() );
+      }
+      catch( BadCommandLineException e )
+      {
+        parser_error( "Could not 'Include' the indicated file" );
+        return false;
+      }
+
+      sync_apitests(apitest,this);
+      apitest.run();
+      sync_apitests(this,apitest);
+
+      total_queries += apitest.total_queries;
+      nonproblematic_queries += apitest.nonproblematic_queries;
 
       return true;
     }
@@ -110,7 +148,7 @@ public class Apitest
 
       if ( !prev_result.contains( expected ) )
       {
-        outln( "(Line "+linenum+") URL: " + prev_href );
+        outln( "("+filename+":"+linenum+") URL: " + prev_href );
         outln( "Expected pattern was not found in the API response!" );
         outln( "" );
         prev_result_unreadable = true;
@@ -171,7 +209,7 @@ public class Apitest
     }
     catch( IOException e )
     {
-      outln( "(Line "+linenum+") URL: " + href );
+      outln( "("+filename+":"+linenum+") URL: " + href );
       outln( "Failed to read API results due to Input/Output Exception" );
       outln( "Details: "+e.getLocalizedMessage() );
       outln( "" );
@@ -214,8 +252,6 @@ public class Apitest
 
   private boolean parse_cmdline( String [] args )
   {
-    sleeptime = 100;
-
     if ( args.length < 1 )
     {
       outln( "Syntax: java Apitest [optional arguments] file" );
@@ -224,8 +260,36 @@ public class Apitest
       return false;
     }
 
-    String filename = args[args.length-1];
+    filename = args[args.length-1];
 
+    return open_file();
+  }
+
+  private static void outln( String str )
+  {
+    System.out.println( str );
+  }
+
+  private void errln( String err )
+  {
+    System.err.println( err );
+  }
+
+  private void parser_error( String err )
+  {
+    if ( linenum != 0 )
+      errln( "(" + filename + ":" + linenum + ") " + err );
+    else
+      errln( "(" + filename + ") " + err );
+  }
+
+  private void parser_error( String err, Throwable e )
+  {
+    parser_error( err + "\nDetails: " + e.getLocalizedMessage() );
+  }
+
+  boolean open_file()
+  {
     try
     {
       is = new FileInputStream( filename );
@@ -247,23 +311,19 @@ public class Apitest
     return true;
   }
 
-  private void outln( String str )
+  class BadCommandLineException extends Exception
   {
-    System.out.println( str );
+    BadCommandLineException()
+    {
+    }
   }
 
-  private void errln( String err )
+  void sync_apitests( Apitest dest, Apitest src )
   {
-    System.err.println( err );
-  }
-
-  private void parser_error( String err )
-  {
-    errln( "(Line " + linenum + ") " + err );
-  }
-
-  private void parser_error( String err, Throwable e )
-  {
-    parser_error( err + "\nDetails: " + e.getLocalizedMessage() );
+    dest.href_base = src.href_base;
+    dest.href_postfix = src.href_postfix;
+    dest.prev_href = src.prev_href;
+    dest.prev_result_unreadable = src.prev_result_unreadable;
+    dest.sleeptime = src.sleeptime;
   }
 }
